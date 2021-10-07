4f24c68ed4cac885a9ebcc3dc9d4ebf00e1754c8
prudenca08 <prudencaahdaffa08@gmail.com>
2021-10-07 22:58:46 +0700

    update api

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"path/filepath"
 	"server_startup/auth"
 	"server_startup/campaign"
 	"server_startup/handler"
@@ -15,8 +14,6 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/multitemplate"
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -29,20 +26,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	db.AutoMigrate(&campaign.Campaign{}, &user.User{}, &payment.Transaction{}, &transaction.Transaction{})
+	db.AutoMigrate(&campaign.Campaign{}, &user.User{}, &payment.Transaction{}, &transaction.Transaction{}, &campaign.CampaignImage{})
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
-	transactionRepository := transaction.NewRepository(db)
+	// transactionRepository := transaction.NewRepository(db)
 
 	userService := user.NewService(userRepository)
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
-	paymentService := payment.NewService()
-	transactionService := transaction.NewService(transactionRepository, campaignRepository, paymentService)
+	// paymentService := payment.NewService()
+	// transactionService := transaction.NewService(transactionRepository, campaignRepository, paymentService)
 
 	userHandler := handler.NewUserHandler(userService, authService)
 	campaignHandler := handler.NewCampaignHandler(campaignService)
-	transactionHandler := handler.NewTransactionHandler(transactionService)
+	// transactionHandler := handler.NewTransactionHandler(transactionService)
 
 	router := gin.Default()
 	router.Use(cors.Default())
@@ -61,10 +58,10 @@ func main() {
 	api.PUT("/campaigns/:id", authMiddleware(authService, userService), campaignHandler.UpdateCampaign)
 	api.POST("/campaign-images", authMiddleware(authService, userService), campaignHandler.UploadImage)
 
-	api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionHandler.GetCampaignTransactions)
-	api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
-	api.POST("/transactions", authMiddleware(authService, userService), transactionHandler.CreateTransaction)
-	api.POST("/transactions/notification", transactionHandler.GetNotification)
+	// api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionHandler.GetCampaignTransactions)
+	// api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
+	// api.POST("/transactions", authMiddleware(authService, userService), transactionHandler.CreateTransaction)
+	// api.POST("/transactions/notification", transactionHandler.GetNotification)
 
 	router.Run()
 }
@@ -111,39 +108,4 @@ func authMiddleware(authService auth.Service, userService user.Service) gin.Hand
 
 		c.Set("currentUser", user)
 	}
-}
-
-func authAdminMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-
-		userIDSession := session.Get("userID")
-
-		if userIDSession == nil {
-			c.Redirect(http.StatusFound, "/login")
-			return
-		}
-	}
-}
-
-func loadTemplates(templatesDir string) multitemplate.Renderer {
-	r := multitemplate.NewRenderer()
-
-	layouts, err := filepath.Glob(templatesDir + "/layouts/*")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	includes, err := filepath.Glob(templatesDir + "/**/*")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for _, include := range includes {
-		layoutCopy := make([]string, len(layouts))
-		copy(layoutCopy, layouts)
-		files := append(layoutCopy, include)
-		r.AddFromFiles(filepath.Base(include), files...)
-	}
-	return r
 }
